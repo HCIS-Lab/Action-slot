00736706e4700948aed4060cc598ebf266abacb9
Hank Kung <hank910140@gmail.com>
2023-11-09 19:51:21 +0800

    Update train_taco.py

--- scripts/train_taco.py
+++ scripts/train_taco.py
@@ -81,7 +81,7 @@ class Engine(object):
             attention_res = (self.model.resolution[0]*args.bg_upsample, self.model.resolution[1]*args.bg_upsample)
         else:
             attention_res = None
-            self.criterion = ActionSlotLoss(args, num_actor_class, attention_res).to(self.args.device)
+        self.criterion = ActionSlotLoss(args, num_actor_class, attention_res).to(self.args.device)
 
         self.cur_epoch = 0
         self.train_loss = []
